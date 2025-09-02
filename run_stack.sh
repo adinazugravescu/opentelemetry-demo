8f872e8706d39562bb8bc44815c88ead8876e1ca
Francesco Andreuzzi <fandreuz@amazon.com>
2025-09-02 22:47:09 +0000

    wip

--- run_stack.sh
+++ run_stack.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-docker kill tempo pyroscope grafana 2>/dev/null || true
-docker rm tempo pyroscope grafana 2>/dev/null || true
-
-docker run --rm \
-        -p 4040:4040 \
-        grafana/pyroscope:main-f6178fd \
-        -distributor.ring.store=inmemory \
-        -ring.store=inmemory \
-        -query-scheduler.ring.store=inmemory \
-        -store-gateway.sharding-ring.store=inmemory \
-        -overrides-exporter.ring.store=inmemory \
-        -compactor.ring.store=inmemory &> pyroscope.out &
-
-docker run --rm -d --name tempo --network host \
-  -v $(pwd)/src/tempo/tempo.yaml:/etc/tempo.yaml \
-  grafana/tempo:2.3.0 \
-  -config.file=/etc/tempo.yaml
+docker kill tempo grafana 2>/dev/null || true
+docker rm tempo grafana 2>/dev/null || true
 
 docker run --rm -d --name grafana --network host \
   -e "GF_SECURITY_ADMIN_USER=admin" \
